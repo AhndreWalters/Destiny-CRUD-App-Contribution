7330af1ef333f64d3beec75714a30a92ae9b271e
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-12-06 21:39:59 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'mysql-640c046-dessyabraham2000-c866.k.aivencloud.com');
-define('DB_NAME', 'crud_app');
+define('DB_SERVER', 'mysql-10dead2a-crudd.g.aivencloud.com');
+define('DB_NAME', 'cruddapp');
 define('DB_USERNAME', 'avnadmin');
-define('DB_PASSWORD', '$2y$10$WsVpM7DyxiXJPb88pp1d2.rc5QlOyXypzrXnhoDfs1DSMZX6mtWCO');
-define('DB_PORT', '12272');
+define('DB_PASSWORD', '$2y$10$hPvLTgiV86nYkTbsg4yV/ep1E6VlfspZkf6jY9i4ybiEFDGsO2Tq.');
+define('DB_PORT', '17808');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
